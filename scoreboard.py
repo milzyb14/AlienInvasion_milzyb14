ade5acefb9123e98e5295105af2f84ed9e04c5f2
Myles Buchanan <107156069+milzyb14@users.noreply.github.com>
2026-04-26 20:40:17 -0400

    Fixed a typo.

--- scoreboard.py
+++ scoreboard.py
@@ -53,7 +53,7 @@ class Scoreboard:
     def prep_high_score(self):
         """Turn the high score into a rendered image."""
         high_score = round(self.stats.high_score, -1)
-        high_score_str = f"HIGH SCORE:{high_score:,}"
+        high_score_str = f"HIGH SCORE: {high_score:,}"
         self.high_score_image = self.font.render(high_score_str, True,
                                                  self.text_color, self.settings.bg_color)
 
